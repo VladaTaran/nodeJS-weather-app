bebf7b617f04b812f0c53fc56205bb106053eae3
Vladislava Vladislava <nakonechna@warefare-tec.com>
2019-09-02 10:28:59 +0300

    http started

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const request = require('request');
+
+const url = 'https://api.darksky.net/forecast/ac8dc22bfe35582031584dc85923246b/37.8267,-122.4233';
+
+request({ url }, (error, response) => {
+    try {
+        const data = JSON.parse(response.body);
+        
+        console.log('response ', data)
+    } catch {
+        console.log(error);
+    }
+})
