52a39282e81d1abab795c65b4599510a71f63c42
Vladislava Vladislava <nakonechna@warefare-tec.com>
2019-09-14 20:16:35 +0300

    provided location

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('New York',(error, data) => {
-    console.log('Error ', error)
-    console.log('Data ', data)
-});
+const address = process.argv[2];
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error ', error);
-    console.log('Data ', data);
-});
+address ? geocode(address,(error, data) => {
+    if(error) {
+        return console.log(error)
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error) {
+            return console.log(error)
+        }
+        console.log(data.location);
+        console.log(forecastData);
+    });
+})
+: console.log('No provided location');
+
